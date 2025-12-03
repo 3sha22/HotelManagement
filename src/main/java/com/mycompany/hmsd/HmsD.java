c007e9dbba07a0f20047a4b401f5098436e8af16
user <user@LAPTOP-80LML9TT>
2025-12-03 21:08:52 +0800

    "initial commit" k

---
+++ src/main/java/com/mycompany/hmsd/HmsD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hmsd;
+
+/**
+ *
+ * @author user
+ */
+public class HmsD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
